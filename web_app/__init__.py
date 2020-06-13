e93048ee16d59f42ef8382694a66719fc06ae0a7
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-06-13 11:07:34 -0400

    read connection cred from env variable

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,6 +1,10 @@
 # web_app/__init__.py
 
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
@@ -8,12 +12,12 @@ from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = "sqlite:///web_app_99.db" # using relative filepath
+DATABASE_URL = os.getenv("DATABASE_URL")  # using relative filepath
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     db.init_app(app)
     migrate.init_app(app, db)
 
